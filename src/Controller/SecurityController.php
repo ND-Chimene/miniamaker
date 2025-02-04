fc2e9c000e60563fcd7334584e102c3453452a92
Shimouna-14 <chimene.ndeby@gmail.com>
2025-02-04 17:01:41 +0100

    Add PageController and update templates for homepage and login page; refactor SecurityController to log user login events with device detection

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,16 +2,37 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\LoginHistory;
+use DeviceDetector\DeviceDetector;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request, EntityManagerInterface $em): Response
     {
+        $deviceDetector = new DeviceDetector($request->headers->get('User-Agent'));
+        $deviceDetector->parse();
+
+        if ($this->getUser()) {
+            $loginHistory = new LoginHistory();
+            $loginHistory
+                ->setUser($this->getUser())
+                ->setIpAddress($request->getClientIp())
+                ->setDevice($deviceDetector->getDevice())
+                ->setOs($deviceDetector->getOs()['name'])
+                ->setBrowser($deviceDetector->getClient()['name'])
+            ;
+            $em->persist($loginHistory);
+            $em->flush();
+        }
+
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
