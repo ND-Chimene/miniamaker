cfa3794bf2432e718157adc8d1953ffc0af4897a
Shimouna-14 <chimene.ndeby@gmail.com>
2025-02-04 10:10:05 +0100

    Redirect authenticated users to profile page in registration process and update email verification success message

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -27,6 +27,10 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager): Response
     {
+        // Si déjà connecté, on redirige vers la page de profil
+        if($this->getUser()) {
+            return $this->redirectToRoute('app_profile');
+        }
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -84,9 +88,8 @@ class RegistrationController extends AbstractController
             return $this->redirectToRoute('app_register');
         }
 
-        // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Votre adresse email a bien été vérifiée. Merci.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_profile');
     }
 }
