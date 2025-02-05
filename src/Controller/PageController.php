53531a3de25a6924a0e81f9ffc2acadc37a1646d
Shimouna-14 <chimene.ndeby@gmail.com>
2025-02-05 10:55:09 +0100

    Add LoginHistoryService to track user login events and log device information

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\LoginHistory;
+use App\Service\LoginHistoryService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class PageController extends AbstractController
 {
     #[Route('/', name: 'app_homepage', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request, LoginHistoryService $lhs): Response
     {
-        if (!$this->getUser()) {
+        if ($this->getUser() && $request->headers->get('referer') === 'https://127.0.0.1:8000/login') {
+            $lhs->addHistory(
+                $this->getUser(),
+                $request->headers->get('user-Agent'),
+                $request->getClientIp()
+            );
+        }
+         if (!$this->getUser()) {
             return $this->render('page/lp.html.twig');
         }
 
