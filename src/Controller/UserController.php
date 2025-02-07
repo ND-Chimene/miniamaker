70956e906709e89c6954f5e38fda02c46a4434f9
Shimouna-14 <chimene.ndeby@gmail.com>
2025-02-07 14:39:24 +0100

    Implement user profile update functionality with form handling and success message

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\UserFormType;
 use Doctrine\ORM\Mapping\Entity;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,10 +12,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class UserController extends AbstractController{
     #[Route('/profile', name: 'app_profile', methods: ['GET', 'POST'])]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $user = $this->getUser();
+        $form = $this->createForm(UserFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($user);
+            $em->flush();
+
+            // Redirection avec flash message
+            $this->addFlash('success', 'Votre profil à été mis à jour');
+            return $this->redirectToRoute('app_profile');
+        }
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'userForm' => $form,
         ]);
     }
 
