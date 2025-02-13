f651d318c256ee9e3b4cacf84e01a7fd082f760e
Shimouna-14 <chimene.ndeby@gmail.com>
2025-02-13 09:28:43 +0100

    Add MessageController and enhance Message entity with lifecycle callbacks

--- src/Entity/Message.php
+++ src/Entity/Message.php
@@ -7,6 +7,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MessageRepository::class)]
+#[ORM\HasLifecycleCallbacks]
 class Message
 {
     #[ORM\Id]
@@ -30,6 +31,17 @@ class Message
 
     #[ORM\Column]
     private ?\DateTimeImmutable $created_at = null;
+
+    public function __construct()
+    {
+        $this->statut = false;
+    }
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue()
+    {
+        $this->created_at = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
@@ -65,7 +77,7 @@ class Message
         return $this->content;
     }
 
-    public function setContent(string $content): static
+    public function setContent(?string $content): static
     {
         $this->content = $content;
 
